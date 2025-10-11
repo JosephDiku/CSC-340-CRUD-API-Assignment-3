ac575add637331cfb2f01d941d9c412b0f89fa3f
Joseph Diku <josephdiku336@gmail.com>
2025-10-11 13:35:33 -0400

    Completed HawkRepo.java according to provided template.

--- src/main/java/com/csc340/CRUDAPI/HawkRepo.java
+++ src/main/java/com/csc340/CRUDAPI/HawkRepo.java
@@ -1,5 +1,20 @@
 package com.csc340.CRUDAPI;
 
-public class HawkRepo {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HawkRepo extends JpaRepository<Hawk, Long> {
+
+    List<Hawk> findHawksByOrigin (String origin);
+   
+    @Query(value = "select * from hawks where population >= ?1", nativeQuery = true)
+    List<Hawk> findHawksByPopulation(int population);
+
+    @Query(value = "select * from hawks where name like %?1%", nativeQuery = true)
+    List<Hawk> findHawksByNameContaining(String name);
 }
+
