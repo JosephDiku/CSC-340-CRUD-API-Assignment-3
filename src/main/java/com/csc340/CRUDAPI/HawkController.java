cbe5abd38d2371b259ec3045dc81266a8f25fbb2
JosephDiku <Josephdiku336@gmail.com>
2025-11-11 14:49:35 -0500

    Changed controller @, created new files for the html from assignment 2, and imported the images from assignment 2

--- src/main/java/com/csc340/CRUDAPI/HawkController.java
+++ src/main/java/com/csc340/CRUDAPI/HawkController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.stereotype.Controller;
 
-@RestController
+//@RestController
+
+@Controller
 public class HawkController {
    
     @Autowired
